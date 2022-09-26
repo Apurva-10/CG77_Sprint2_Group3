3115799d349ed39eaf0881907bbeb14c73c445de
apurva d <85248357+Apurva-10@users.noreply.github.com>
2022-09-26 19:17:47 +0530

    Add files via upload

---
+++ CG77_sprint2_group3/CUT/CODE/Header/header.h
@@ -0,0 +1,20 @@
+/*************************************************
+
+Filename - header.h
+
+Description - containing header files
+
+**************************************************/
+
+#ifndef HEADER_H
+#define HEADER_H
+#include <bits/stdc++.h>
+#include <fstream>
+#include <termios.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <thread>
+#include <mutex>
+#include "functions.cpp"
+#endif
+
